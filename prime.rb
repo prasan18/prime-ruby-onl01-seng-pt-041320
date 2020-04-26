88d4155b8f840a48955faa8423700621c51acbc9
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-26 20:07:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,9 @@ def prime?(num)
     return false if !num.integer?
     return false if num <2
      return true  if num ==2
-     (2..num-1).each{|int|return false if %int ==0}
-      
+     (2..num-1).each{|int|return false if % int ==0}
+     end 
     true
-end
 end
 end
 end
